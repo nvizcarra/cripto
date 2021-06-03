dff0ebccbd040774c9b27809cdab7b74743304bf
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-03 20:51:29 -0300

    Create Form Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import imagen from './cryptomonedas.png';
+import Formulario from './components/Formulario';
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -48,6 +49,8 @@ function App() {
         <Heading>
           Cotiza Criptomonedas al Instante
         </Heading>
+        
+        <Formulario/>
       </div>
     </Contenedor>
   );
