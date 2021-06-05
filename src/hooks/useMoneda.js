429c13e7246600ef69ee1076cb68944ef9bd59a5
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-05 00:33:07 -0300

    Styled components for custom hook

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,6 +1,27 @@
 // El Fragment lo agrego cuando creo la constante Seleccionar, ya que ahí lo voy a utilizar
 // El useState lo necesito para el custom hook
 import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display:block;
+    padding: 1rem;
+    -webkit-appearance:none;
+    border-radius: 10px;
+    border:none;
+    font-size: 1.8rem;
+`;
 
 // Este código es el state y las operaciones que podamos realizar
 const useMoneda = (label, stateInicial, opciones) => {
@@ -11,7 +32,7 @@ const useMoneda = (label, stateInicial, opciones) => {
         const Seleccionar = () => (
             <Fragment>
                 {/* Con este Fragment lo que quiero hacer es retornar el select y el label */}
-                <label>{label}</label>
+                <Label>{label}</Label>
                 <select
                     onChange={ e => actualizarState(e.target.value)}
                     value={state}
