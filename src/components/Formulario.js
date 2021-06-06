c4fbd197f724b15b8de8523fb39dab2b4cb36e6a
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-06 13:54:52 -0300

    Create a 2° custom hook

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 // Luego de crear el hook en useMoneda, lo importo acá
 import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
 
 const Boton = styled.input`
     margin-top: 20px;
@@ -38,12 +39,17 @@ const Formulario = () => {
     // Es Array destructuring y estamos estipulando el orden en el que se retornan
     const [moneda, SelectMonedas] = useMoneda('Elige tu moneda', '', MONEDAS);
 
+    // Utilizar useCriptomoneda
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '');
+
     return (
 
         <form>
             {/* Utilizo SelectMonedas como componente */}
-            <SelectMonedas/>
+            <SelectMonedas />
             
+            <SelectCripto />
+
             <Boton
                 type="submit"
                 value="Calcular"
---
+++ src/hooks/useCriptomoneda.js
@@ -0,0 +1,53 @@
+// El Fragment lo agrego cuando creo la constante Seleccionar, ya que ahí lo voy a utilizar
+// El useState lo necesito para el custom hook
+import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display:block;
+    padding: 1rem;
+    -webkit-appearance:none;
+    border-radius: 10px;
+    border:none;
+    font-size: 1.2rem;
+`;
+
+// Este archivo js es copia de useMoneda.js. La diferencia es que las opciones las vamos a traer desde una API
+const useCriptomoneda = (label, stateInicial, opciones) => {
+    // State para el custom hook
+    const [state, actualizarState] = useState('');
+
+        // Este código es la función que se mostrará en pantalla
+        const SelectCripto = () => (
+            <Fragment>
+                {/* Con este Fragment lo que quiero hacer es retornar el select y el label */}
+                <Label>{label}</Label>
+                <Select
+                    onChange={ e => actualizarState(e.target.value)}
+                    value={state}
+                >
+                    {/* <option value="">-- Seleccione su moneda --</option>
+                    {opciones.map(opcion => (
+                        <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+                    ))} */}
+                </Select>
+            </Fragment>
+        );
+
+        // Returnar state, interfaz y función que modifica el state
+        return [state, SelectCripto, actualizarState ];
+}
+
+export default useCriptomoneda;
+
